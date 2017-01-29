5ef394d8173bc6a46f55adf9508c91aaa94f66d0
Ordanis <ordanisanchez@gmail.com>
2017-01-28 23:32:42 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -10,11 +10,12 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    # Package requirements here
+    "Jinja2==2.9.5"
 ]
 
 test_requirements = [
-    # TODO: put package test requirements here
+    # Package test requirements here
 ]
 
 setup(
@@ -34,18 +35,12 @@ setup(
     install_requires=requirements,
     license="MIT license",
     zip_safe=False,
-    keywords='HtmlTestRunner',
+    keywords='HtmlTestRunner, TestRunner, Html Reports',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
