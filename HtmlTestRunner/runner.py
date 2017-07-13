6d45315909cf243175b27f9814e7bc2d2ccf2bb8
Leo Lee <mail@debugtalk.com>
2017-07-13 16:51:18 +0800

    bugfix: 0:00:12 should be 7 digits

--- HtmlTestRunner/runner.py
+++ HtmlTestRunner/runner.py
@@ -60,7 +60,7 @@ class HTMLTestRunner(TextTestRunner):
             self.stream.writeln(result.separator2)
             run = result.testsRun
             self.stream.writeln("Ran {} test{} in {}".format(run,
-                                run != 1 and "s" or "", str(self.time_taken)[:5]))
+                                run != 1 and "s" or "", str(self.time_taken)[:7]))
             self.stream.writeln()
 
             expectedFails = len(result.expectedFailures)
